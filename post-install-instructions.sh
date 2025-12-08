a151d155b42b131f2c8219c7702f251ae79719ff
Nico Russo <nicojrusso@gmail.com>
2025-12-07 22:31:58 -0500

    added kitty instructions to post-install

--- post-install-instructions.sh
+++ post-install-instructions.sh
@@ -6,3 +6,4 @@ echo
 echo
 
 echo "In Neovim Extras (x), install util.dot, util.mini-hipatterns, lang.clangd, lang.cmake, lang.git, lang.go, lang.markdown, lang.python, and lang.typescript"
+echo "In kitty terminal, kitten choose-fonts, kitten themes"
